ecb885f290793bf4114e9a3d6353f8a2f5cfb720
Katelyn <godfrey.katelyn@gmail.com>
2015-11-25 11:16:54 -0500

    UI tweaks and QCAR enable btn

---
+++ Assets/Scripts/ReviveQCAR.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ReviveQCAR : MonoBehaviour {
+
+	private QCARBehaviour QCAR;
+
+
+	// Use this for initialization
+	void Start () {
+		QCAR = FindObjectOfType<QCARBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartTracking() {
+		QCAR.enabled = true;
+		ObjectTracker it = TrackerManager.Instance.GetTracker<ObjectTracker>();
+		MarkerTracker mt = TrackerManager.Instance.GetTracker<MarkerTracker>();
+		CameraDevice.Instance.Start ();
+		it.Start ();
+		mt.Start ();
+	}
+
+	public void StopTracking() {
+		QCAR.enabled = false;
+		ObjectTracker it = TrackerManager.Instance.GetTracker<ObjectTracker>();
+		MarkerTracker mt = TrackerManager.Instance.GetTracker<MarkerTracker>();
+		it.Stop ();
+		mt.Stop ();
+		CameraDevice.Instance.Stop ();
+	}
+
+}
